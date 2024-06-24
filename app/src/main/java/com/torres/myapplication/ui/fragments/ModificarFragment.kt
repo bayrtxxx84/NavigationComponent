79ad9e205879ea9ab0b0697fb5937b8928ce37d3
Bayron Torres <torres.bayron@gmail.com>
2024-06-24 18:38:30 -0500

    Proyecto migrado a navigation component conarquitectura SingleActivity

---
+++ app/src/main/java/com/torres/myapplication/ui/fragments/ModificarFragment.kt
@@ -0,0 +1,21 @@
+package com.torres.myapplication.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.torres.myapplication.R
+
+
+class ModificarFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_modificar, container, false)
+    }
+
+}
